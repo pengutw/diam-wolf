ad683a579f8732f3d2d926ef665289127cc36eaf
pengutw <131533416+pengutw@users.noreply.github.com>
2025-07-16 19:14:24 +0800

    Add files via upload

---
+++ lang/cht/index.php
@@ -0,0 +1,41 @@
+<?php
+
+//***********************************************************
+//index
+//***********************************************************
+$lang_title="汝等是人是狼？";
+$lang_menu_title="選單";
+$lang_menu_script_info="系統特點";
+$lang_menu_rule="遊戲規則";
+$lang_menu_version="版本紀錄";
+$lang_menu_old_log="紀錄閱覽";
+$lang_menu_icon_view="頭像一覽";
+$lang_menu_icon_upload="頭像登錄";
+$lang_create_room_disable="新村莊的建立被管理員禁用";
+
+//***********************************************************
+//room_manager
+//***********************************************************
+
+$lang_room_htmltitle="建立村莊";
+$lang_room_check_table_fail="資料庫表格缺損，需要安裝";
+$lang_room_fail="建立新村莊失敗";
+$lang_room_fail_input="請確認下列項目<li>村名跟村說明都要有內容<li>最大人數必須在有效的數字範圍之內";
+$lang_create_room_disable_maxroom_f="不能允許同時存在超過 ";
+$lang_create_room_disable_maxroom_b=" 個村莊";
+$lang_room_tryback="回上一頁";
+
+//***********************************************************
+//upgrade
+//***********************************************************
+
+$lang_upgrade_title="安裝及升級";
+$lang_upgrade_start="自動安裝及升級作業 開始";
+$lang_upgrade_changeset="尚未更新 'setting.php' 的內容";
+$lang_upgrade_check_charset="正在檢查既存TABLE的編碼(錯誤的編碼可能導致文字亂碼, 且不可藉由本工具修正)";
+$lang_upgrade_warn_charset="下列TABLE使用了UTF8以外的編碼: ";
+$lang_upgrade_set_sql="嘗試設定 SQL_MODE(就算錯了也沒有關係)";
+$lang_upgrade_alter_table="將下列TABLE更新到最新的結構: ";
+$lang_upgrade_create_table="正在創造下列TABLE: ";
+$lang_upgrade_end="自動安裝及升級作業 結束";
+?>
---
+++ socket/websockets.php
@@ -0,0 +1,52 @@
+#!/usr/bin/env php
+<?php
+require_once(__DIR__.'/../functions.php');
+
+$server = new Swoole\WebSocket\Server("0.0.0.0", 8443, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+$server->set([
+    'reactor_num' => 24,
+    'worker_num' => 12,
+    'max_request' => 10000,
+    'ssl_cert_file' => "fullchain.pem",
+    'ssl_key_file'  => "privkey.pem",
+    'ssl_protocols' => SWOOLE_SSL_TLSv1_2 | SWOOLE_SSL_TLSv1_3 | SWOOLE_SSL_TLSv1_1 | SWOOLE_SSL_SSLv2,
+    'log_level' => SWOOLE_LOG_ERROR,
+    'user' => 'www-data',
+    'group' => 'www-data',
+]);
+
+$table = new Swoole\Table(1024);
+$table->column('fd', Swoole\Table::TYPE_INT);
+$table->column('room_no', Swoole\Table::TYPE_INT);
+$table->column('gameuserid', Swoole\Table::TYPE_INT);
+$table->create();
+$server->table = $table;
+
+$server->on('open', function (Swoole\WebSocket\Server $_server, $request): void {
+	global $server;
+	[$room_no, $gameuserid] = explode("\t", authcode(str_replace("/wss/","",(string) $request->server['request_uri']), 'DECODE'));
+    $server->room_no = $room_no;
+    $server->table->set($request->fd, ['fd' => $request->fd, 'room_no' => $room_no, 'gameuserid' => $gameuserid]);
+});
+
+$server->on('message', function (Swoole\WebSocket\Server $_server, $frame): void {
+	global $server;
+	
+	foreach ($server->table as $key => $value) {
+		if (!$server->isEstablished($value["fd"])) {
+			$server->table->del($value["fd"]);
+        }
+    }
+	
+	foreach ($server->table as $value) {
+	 	 if ($value["room_no"] == $server->room_no) {
+			$server->push($value["fd"], "Last updated: ".gmdate("H:i:s",time()+3600*8));
+         }
+	}
+});
+
+$server->on('close', function ($_server, $fd): void {
+});
+
+$server->start();
+?>
